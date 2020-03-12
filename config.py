3e7f230e27b917b12d868973e7f58f019a7d2748
petr.kalis <petr.kalis@gmail.com>
2020-03-12 10:21:00 +0100

    Skeleton for config.py

--- config.py
+++ config.py
@@ -2,3 +2,9 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key' # or part only for dev!
+    FLASKS3_BUCKET_NAME = ''
+    AWS_REGION = ''
+    AWS_ACCESS_KEY_ID = ''
+    AWS_SECRET_ACCESS_KEY = ''
+    FLASKS3_DEBUG = True
+    DYNAMODB_ENDPOINT = ''
